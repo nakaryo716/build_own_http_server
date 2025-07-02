b67f4118bd2453c9e603e3c9745cad4644a4f892
ryoNAKAYA <nakayaryo0716@gmail.com>
2025-07-02 23:21:09 +0900

    refactor: use ThreadPool to handle http request

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,23 @@ use std::{
     fs,
     io::{BufRead, BufReader, Write},
     net::TcpStream,
+    thread,
+    time::Duration,
 };
+
+use build_own_http_server::ThreadPool;
 
 fn main() {
     let listener = std::net::TcpListener::bind("127.0.0.1:8080").unwrap();
+    let pool = ThreadPool::new(4);
+
     println!("Server is running: http://localhost:8080");
 
     loop {
         let (stream, _) = listener.accept().unwrap();
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
@@ -23,10 +31,13 @@ fn handle_connection(mut stream: TcpStream) {
         .take_while(|v| !v.is_empty())
         .collect();
 
-    let (status_line, file_name) = if http_req[0] == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "hello.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    let (status_line, file_name) = match http_req[0].as_str() {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "hello.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
     };
 
     let contents = fs::read_to_string(file_name).unwrap();
